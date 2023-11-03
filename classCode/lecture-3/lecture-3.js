f52a3655d73fe62b7bb03923f114f1feef4bcd55
rajalino-arcada <rajalino@arcada.fi>
2023-11-03 10:57:07 +0200

    Change Lecture 3

--- classCode/lecture-3/lecture-3.js
+++ classCode/lecture-3/lecture-3.js
@@ -15,5 +15,28 @@ function buttonClick() {
     // Works as well, note "name" not "#name"
     //      document.getElementById("name").value;
 
-    alert(`Hello ${name}!`);
+    document.querySelector("#hello-output").innerText = `Hello ${name}!`;
+}
+
+function buttonClickForTimeTrained() {
+    console.log("buttonClick");
+    const userInput = document.querySelector("#time-trained").value;
+
+    timeTrained = Number(userInput);
+
+    let max = 60;
+    let min = 15;
+
+    if ( timeTrained === max ) {
+        document.querySelector("#personal-trainer-output").innerText = `You trained for: ${timeTrained} minutes. Good job! Remember to stretch and rest in between!`;
+    }
+    else if ( timeTrained >= 90 ) {
+        document.querySelector("#personal-trainer-output").innerText = `You trained for: ${timeTrained} minutes. It's good that you like training but now it is TIME TO REST!`;
+    }
+    else if ( timeTrained < min ) {
+        document.querySelector("#personal-trainer-output").innerText = `Come on now, you should train atleast 15 minutes. YOU CAN DO IT!`;
+    }
+    else {
+        document.querySelector("#personal-trainer-output").innerText = `You trained for: ${timeTrained} minutes. Good, but you can do better!`;
+    }
 }
